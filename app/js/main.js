88fbeb8f524dee3b99a9986096d45f0bc9739e44
AESalkov <a.e.salkov@gmail.com>
2020-04-21 16:52:10 +0700

    added contact, settings

--- app/js/main.js
+++ app/js/main.js
@@ -48,10 +48,10 @@ $(function(){
 
     });
 
-    $('.product_one-tabs .tab').on('click', function(event) {
+    $('.product_one-tabs .tab, settings__tabs .tab').on('click', function(event) {
         var id = $(this).attr('data-id');
-            $('.product_one-tabs').find('.tab-item').removeClass('active-tab').hide();
-            $('.product_one-tabs .tabs').find('.tab').removeClass('active');
+            $('.product_one-tabs, settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product_one-tabs .tabs, settings__tabs .tabs').find('.tab').removeClass('active');
             $(this).addClass('active');
             $('#'+id).addClass('active-tab').fadeIn();
             return false;
